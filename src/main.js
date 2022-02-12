94c5a076dadb12011e6dffd3a9d5c9c06947f501
Vladislav Stepanenko <traumerei96@gmail.com>
2022-02-12 23:16:29 +1000

    Add component menu

---
+++ src/main.js
@@ -0,0 +1,16 @@
+setTimeout(function timeout() {
+  console.log('Таймаут');
+}, 0);
+
+let p = new Promise(function(resolve, reject) {
+  console.log('Создание промиса');
+  
+  
+  resolve();
+});
+
+p.then(function(){
+  console.log('Обработка промиса');
+});
+
+console.log('Конец скрипта');
